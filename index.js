14847e8cc63411893ae3fe87c06d32edb565e6bf
Austin Kelleher <austin.kell47@gmail.com>
2017-09-22 10:41:18 -0400

    Fixes #1 - Support Lasso v3

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var CSON = require('season');
 
 module.exports = function(lasso, config) {
@@ -5,19 +7,24 @@ module.exports = function(lasso, config) {
             object: true,
 
             read: function(path, lassoContext, callback) {
+                return new Promise((resolve, reject) => {
+                    callback = callback || function (err, res) {
+                        return err ? reject(err) : resolve(res);
+                    };
 
-                CSON.readFile(path, function(err, o) {
-                    if (err) {
-                        return callback(err);
-                    }
+                    CSON.readFile(path, function(err, o) {
+                        if (err) {
+                            return callback(err);
+                        }
 
 
-                    callback(null, JSON.stringify(o));
-                });
+                        callback(null, JSON.stringify(o));
+                    });
+                })
             },
 
             getLastModified: function(path, lassoContext, callback) {
-                lassoContext.getFileLastModified(path, callback);
+                return lassoContext.getFileLastModified(path, callback);
             }
         });
 };
